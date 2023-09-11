178c64e859642ffb61aae80d70e9dc96278adaa0
Almighty Tronny <tronnyg@gmail.com>
2023-09-11 15:25:56 +0200

    Base Commit WPF
    Created Project

---
+++ Yugen Naidoo ST10047403 PROG6212 POE PART 1/Yugen Naidoo ST10047403 PROG6212 POE PART 1/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Yugen_Naidoo_ST10047403_PROG6212_POE_PART_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
